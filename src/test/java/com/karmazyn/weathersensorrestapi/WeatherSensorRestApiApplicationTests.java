2d55dcf5dbebd474d1b35bc1f57253aadf0c143c
Alex Karmazyn <karmazino548@gmail.com>
2024-03-25 22:23:05 +0100

    Add clear project with dependencies and db

---
+++ src/test/java/com/karmazyn/weathersensorrestapi/WeatherSensorRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karmazyn.weathersensorrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherSensorRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
